0c2187211ad279738ea8a34b7cee804dc27706d0
TIMO BACHMANN <timo.bachmann@student.ie.edu>
2021-01-25 15:27:00 +0100

    feature 1 timestamp added to app_time.py

--- app_time.py
+++ app_time.py
@@ -1,4 +1,9 @@
 from datetime import date
+from datetime import datetime
 
 today = date.today()
 print("Today it is: ",today)
+
+time_now = datetime.now()
+print("Timestamp: ",time_now.strftime("%H:%M:%S"))
+
